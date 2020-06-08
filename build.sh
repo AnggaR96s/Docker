1e2f3f07a1b96b774a4382e0450463f1deb55210
Angga <angga@linuxmail.org>
2020-06-08 18:33:02 +0700

    initial commit
    Change-Id: I6c5bc61404d34ef7c9f2a924c0ac1a1bd38cdbbc
    Signed-off-by: Angga <angga@linuxmail.org>

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+exit_script()
+{
+echo "Build was killed!"
+sendTG "Docker image build was killed!"
+}
+
+trap exit_script SIGINT SIGTERM
+
+function sendTG() {
+    curl -s "https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendmessage" --data "text=${*}&chat_id=-1001372533112&parse_mode=Markdown"
+}
+
+sendTG "\`Docker image is being updated!\`"
+
+docker build . -t dasbastard/test:latest
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push dasbastard/test
+
+sendTG "\`I have pushed new images to docker\` %0A [Images are Here](https://hub.docker.com/r/dasbastard/test)"
